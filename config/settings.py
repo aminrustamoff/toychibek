2498c54e4a28878fe9c51f7db2a16361d865ed46
Aminjon <aminrustamov0424@gmail.com>
2026-05-09 22:11:31 +0500

    500 error recue

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&=c^&b@c(@wgavpu#!-f%lyge39w=leh(8lxq(i_w5ohzi=gei'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [ '*' ]
 
